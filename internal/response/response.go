84c0a6bd2d0e75b65c1621d29265cc8b714653de
Marcello Fabrizio <marcfabrizio10@gmail.com>
2025-07-11 16:31:26 -0300

    writer struct

--- internal/response/response.go
+++ internal/response/response.go
@@ -14,22 +14,47 @@ const (
 	StatusInternalServerError StatusCode = 500
 )
 
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+type Writer struct {
+	ioWriter io.Writer
+}
+
+func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
 	httpVersion := "HTTP/1.1"
 	switch statusCode {
 	case StatusOK:
-		_, err := w.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusOK, "OK")))
+		_, err := w.ioWriter.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusOK, "OK")))
 		return err
 	case StatusBadRequest:
-		_, err := w.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusBadRequest, "Bad Request")))
+		_, err := w.ioWriter.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusBadRequest, "Bad Request")))
 		return err
 	case StatusInternalServerError:
-		_, err := w.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusInternalServerError, "Internal Server Error")))
+		_, err := w.ioWriter.Write([]byte(fmt.Sprintf("%s %d %s\r\n", httpVersion, StatusInternalServerError, "Internal Server Error")))
 		return err
 	default:
-		_, err := w.Write([]byte(fmt.Sprintf("%s %d\r\n", httpVersion, statusCode)))
+		_, err := w.ioWriter.Write([]byte(fmt.Sprintf("%s %d\r\n", httpVersion, statusCode)))
 		return err
 	}
+}
+
+func (w *Writer) WriteHeaders(headers headers.Headers) error {
+
+	for k, v := range headers {
+		_, err := w.ioWriter.Write([]byte(fmt.Sprintf("%s: %s\r\n", k, v)))
+		if err != nil {
+			return err
+		}
+	}
+	_, err := w.ioWriter.Write([]byte("\r\n"))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (w *Writer) WriteBody(p []byte) (int, error) {
+	return w.ioWriter.Write(p)
 }
 
 func GetDefaultHeaders(contentLen int) headers.Headers {
@@ -39,21 +64,4 @@ func GetDefaultHeaders(contentLen int) headers.Headers {
 	headers.Parse([]byte("Content-Type: text/plain\r\n"))
 
 	return headers
-}
-
-func WriteHeaders(w io.Writer, headers headers.Headers) error {
-
-	for k, v := range headers {
-		_, err := w.Write([]byte(fmt.Sprintf("%s: %s\r\n", k, v)))
-		if err != nil {
-			return err
-		}
-	}
-	_, err := w.Write([]byte("\r\n"))
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
